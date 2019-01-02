ecea38a50f94882091dd2e24f2a80c0be6432f4d
陈鹏 <383559698@qq.com>
2019-01-02 17:45:10 +0800

    rm fragment_result

--- example_fragment_proxy_activity_result/src/main/java/com/simple/example_fragment_proxy_activity_result/OnResultListener.java
+++
@@ -1,7 +0,0 @@
-package com.simple.example_fragment_proxy_activity_result;
-
-import android.content.Intent;
-
-public interface OnResultListener {
-    void onActivityResult(int requestCode, int resultCode, Intent data);
-}
