9f3afcf5fa46de4d0afa835920ddd7cd1bba6179
陈鹏 <383559698@qq.com>
2019-01-02 17:44:13 +0800

    rm make sign

---
+++ example_image_swiper/src/main/java/com/simple/example_image_swiper/MainActivity.java
@@ -0,0 +1,17 @@
+package com.simple.example_image_swiper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
