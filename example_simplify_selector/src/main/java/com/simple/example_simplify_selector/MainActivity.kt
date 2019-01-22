9e6ea25a68cda21f4c9fadbccddea4465c90328a
陈鹏 <383559698@qq.com>
2019-01-22 13:42:14 +0800

    add example_simplify_selector

---
+++ example_simplify_selector/src/main/java/com/simple/example_simplify_selector/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.simple.example_simplify_selector
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
