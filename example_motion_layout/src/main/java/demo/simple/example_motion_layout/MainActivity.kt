1065ce8a708171e6a3be37ff98996358d9ecce5c
陈鹏 <simple19930611@gmail.com>
2020-06-15 10:35:34 +0800

    add:example_motion_layout

---
+++ example_motion_layout/src/main/java/demo/simple/example_motion_layout/MainActivity.kt
@@ -0,0 +1,12 @@
+package demo.simple.example_motion_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
