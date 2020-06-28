12380c3b14fd5c18e14842ee4a8aae561f8abe20
陈鹏 <simple19930611@gmail.com>
2020-06-28 16:10:05 +0800

    update:example_ffmpeg extrea c

--- example_ffmpeg/src/main/cpp/FFmpegUtils.cpp
+++ example_ffmpeg/src/main/cpp/FFmpegUtils.cpp
@@ -1,10 +1,11 @@
 #include <jni.h>
 
-
 extern "C" {
+#include <libavutil/avutil.h>
+
 JNIEXPORT jstring JNICALL
 Java_demo_simple_example_1ffmpeg_MainActivity_getVersion(JNIEnv *env, jclass clazz) {
-    char *version = "hello";
+    const char *version = av_version_info();
     return env->NewStringUTF(version);
 }
 
--- example_ffmpeg/src/main/java/demo/simple/example_ffmpeg/MainActivity.java
+++ example_ffmpeg/src/main/java/demo/simple/example_ffmpeg/MainActivity.java
@@ -3,9 +3,12 @@ package demo.simple.example_ffmpeg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tvVersion = findViewById(R.id.tvVersion);
-        tvVersion.setText(getVersion());
+        String version = String.format("ffmpeg version == %s", getVersion());
+        tvVersion.setText(version);
+
+        Log.d(TAG, version);
     }
 
     static {
         System.loadLibrary("utils");
+        System.loadLibrary("avutil");
     }
 
     public static native String getVersion();
