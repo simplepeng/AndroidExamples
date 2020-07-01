bb8da7c67030309e3d8d0e8cafc7d7ac5e792b4c
陈鹏 <simple19930611@gmail.com>
2020-07-01 18:32:39 +0800

    update:example_ffmpeg

---
+++ example_ffmpeg/src/main/cpp/ffmpeg_utils.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+
+extern "C" {
+#include <libavutil/avutil.h>
+
+JNIEXPORT jstring JNICALL
+Java_demo_simple_example_1ffmpeg_MainActivity_getVersion(JNIEnv *env, jclass clazz) {
+    const char *version = av_version_info();
+    return env->NewStringUTF(version);
+}
+
+}
